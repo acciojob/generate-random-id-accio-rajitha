cf91a42e3450f1b31369da5bb361f7c87ffcea59
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2024-03-29 15:45:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
   // write your code here
+	var text = "";
+	var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for(var i =0;i<=l; i++){
+		res +=char_list.charAt(
+			Maath.floor(
+			Math.random*char_list.length;
+		     )
+		)
+	}
+	return res;
 }
 
 // Do not change the code below.
